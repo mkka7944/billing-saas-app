e3326920a9cbdfd7062fa45730984af2c3cd88d2
mkka7944 <kashifkhalil74@gmail.com>
2026-06-20 15:46:35 +0500

    remove staff-light-mode override so staff can use dark/light themes like admin

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -228,7 +228,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
   }, [handleResize])
 
   return (
-    <div className={cn("flex h-full", roleName === 'field_staff' && 'staff-light-mode')}>
+    <div className="flex h-full">
       <BillingSidebar />
 
       <div className="flex-1 flex flex-col min-w-0">
